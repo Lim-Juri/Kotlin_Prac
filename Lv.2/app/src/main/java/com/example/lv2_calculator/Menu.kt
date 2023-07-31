34c42cd1b94b798804e40383991d791ace5e8533
Lim-Juri <imjuly05@gmail.com>
2023-07-31 13:53:23 +0900

    주사위 만들기

--- Lv.2/app/src/main/java/com/example/lv2_calculator/Menu.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lv2_calculator
-
-fun main() {
-    println("========================Welcome=========================")
-    println("[1] Burger Menu, [2] Side Menu, [3] Drink Menu, [4] 종료")
-
-}
