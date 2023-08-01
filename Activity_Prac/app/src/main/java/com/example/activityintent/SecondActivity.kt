aaaad69b5aa9c522e8859665650fe6b9ddb77fca
Lim-Juri <imjuly05@gmail.com>
2023-08-01 21:09:31 +0900

    액티비티 파일 만들기

--- Activity_Prac/app/src/main/java/com/example/activityintent/SecondActivity.kt
+++ Activity_Prac/app/src/main/java/com/example/activityintent/SecondActivity.kt
@@ -3,16 +3,50 @@ package com.example.activityintent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        Log.i(TAG, "onCreate")
 
         val button1 = findViewById<Button>(R.id.button_close)
         button1.setOnClickListener {
             finish()
         }
+    }
+
+    val TAG = "SecondActivity_Lifrcycle"
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestory()")
     }
 }
