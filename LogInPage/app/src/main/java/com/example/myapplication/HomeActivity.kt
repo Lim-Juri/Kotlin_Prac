b61b79d2cea0fa4bc958dda0175c554f72690ffe
Lim-Juri <imjuly05@gmail.com>
2023-08-07 19:40:59 +0900

    완성!

--- LogInPage/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ LogInPage/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,27 +1,46 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val clickend = findViewById<Button>(R.id.finish)
+
+        val clickend = findViewById<ConstraintLayout>(R.id.finish)
         val strData = intent.getStringExtra("dataFromSignInActivity")
         val editID = findViewById<TextView>(R.id.textView)
         editID.text = getString(R.string.id_format, strData)
 
-        clickend.setOnClickListener{
-            val clickintent = Intent(this, SignInActivity::class.java)
-            startActivity(clickintent)
+        clickend.setOnClickListener {
+            finish()
         }
+
+        val imageLayout = findViewById<ConstraintLayout>(R.id.back_all)
+        val image = when ((1..5).random()) {
+            1 -> R.drawable.cat
+            2 -> R.drawable.dog
+            3 -> R.drawable.dog2
+            4 -> R.drawable.dog3
+            5 -> R.drawable.img
+            else -> R.drawable.cat
+        }
+
+        imageLayout.setBackgroundResource(image)
 
     }
 }
--- LogInPage/app/src/main/java/com/example/myapplication/SignInActivity.kt
+++ LogInPage/app/src/main/java/com/example/myapplication/SignInActivity.kt
@@ -3,22 +3,34 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var activityResultLauncher : ActivityResultLauncher<Intent>
         val edit_id = findViewById<EditText>(R.id.edit1)
         val edit_password = findViewById<EditText>(R.id.edit2)
-
         val log = findViewById<Button>(R.id.button)
-        log.setOnClickListener {
 
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val bb = it.data?.getStringExtra("id") ?: ""
+                    edit_id.setText(bb)
+                    val aa = it.data?.getStringExtra("password") ?: ""
+                    edit_password.setText(aa)
+                }
+            }
+
+        log.setOnClickListener {
 
             if (edit_id.text.toString().isBlank() && edit_password.text.toString().isBlank()) {
                 Toast.makeText(
@@ -44,6 +56,7 @@ class SignInActivity : AppCompatActivity() {
                     "로그인 성공",
                     Toast.LENGTH_SHORT
                 ).show()
+
                 val logintent = Intent(this, HomeActivity::class.java)
                 logintent.putExtra("dataFromSignInActivity", edit_id.text.toString())
                 startActivity(logintent)
@@ -52,8 +65,13 @@ class SignInActivity : AppCompatActivity() {
 
         val join = findViewById<Button>(R.id.button2)
         join.setOnClickListener {
-            val joinintent = Intent(this, SignpActivity::class.java)
-            startActivity(joinintent)
+////            val joinintent = Intent(this, SignpActivity::class.java)
+//            startActivity(joinintent)
+
+            val intent = Intent(this, SignUpActivity::class.java)
+            activityResultLauncher.launch(intent)
+
         }
     }
+
 }
