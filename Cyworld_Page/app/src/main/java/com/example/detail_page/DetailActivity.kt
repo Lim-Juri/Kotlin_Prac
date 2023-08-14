d606949954854406f6b8a244e713251cc7fe6d6f
Lim-Juri <imjuly05@gmail.com>
2023-08-14 14:35:00 +0900

    cyworld design

---
+++ Cyworld_Page/app/src/main/java/com/example/detail_page/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.detail_page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_activity)
+    }
+}
--- PRT/app/src/main/java/com/example/prt/Memo.kt
+++ PRT/app/src/main/java/com/example/prt/Memo.kt
@@ -2,18 +2,14 @@ package com.example.prt
 
 
 fun main() {
-    val n = readLine()!!.toInt()
-    val k = readLine()!!.toInt()
-    val result = n * 12000 + k * 2000 - n / 10 * 2000
-    println("$result")
-}
-
-
-class Solution {
-    fun solution(n: Int, k: Int): Int {
-        return (n * 12000) + (k * 2000) - (n / 10 * 2000)
+    for (i in 1..100) {
+        if (i % 5 == 0) {
+            println(i)
+        }
     }
 }
+
+
 
 
 
